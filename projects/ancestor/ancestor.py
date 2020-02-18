3af726bea56c61d8e20e7868bca9eb57dde732c5
Krishan Nattar <krishan.nattar@gmail.com>
2020-02-18 13:04:59 -0800

    now i have two ways of solving earliest ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -12,10 +12,104 @@ class Stack():
     def size(self):
         return len(self.stack)
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+# def earliest_ancestor(ancestors, starting_node):
+#     # Answer will hold the earliest ancestor answer[0] and the amount of edges to reach it answer[1]
+#     answer = [None, None]
+
+#     # Adjacency list for all nodes/edges in ancestors
+#     ancestor_dictionary = {}
+
+#     # Add all nodes and edges to adjacency list
+#     for a in ancestors:
+#         if a[0] in ancestor_dictionary:
+#             ancestor_dictionary[a[0]].add(a[1])
+#         else:
+#             ancestor_dictionary[a[0]] = set()
+#             ancestor_dictionary[a[0]].add(a[1])
+
+#     # Set up a reverse depth first search of the nodes.
+#     stack = Stack()
+#     visited = set()
+#     stack.push(starting_node)
+#     while stack.size() > 0:
+#         child = stack.pop()
+
+
+#         # If we are currently not on the first node, do a top down DFS of the nodes to find the length of any connecting path to the original child
+#         if child != starting_node:
+
+#             while True:
+#                 stack_2 = Stack()
+#                 visited_2 = set()
+#                 stack_2.push([child])
+#                 while stack_2.size() > 0:
+#                     current_path = stack_2.pop()
+#                     current_node = current_path[-1]
+                    
+#                     #Here we have reached the base node
+#                     if current_node == starting_node:
+#                         # If nothing has been put in answer, we use the current value of 'child' as our earliest ancestor.
+#                         # We also set the length of the path to indicate how many edges there are to get there.
+#                         if answer[0] is None:
+#                             answer[0] = child
+#                             answer[1] = len(current_path)
+                        
+#                         # If an answer already exists, we check to see if the current path is longer than the existing answer.
+#                         # If the current path is longer, we overwrite the ancestor and edge length
+#                         elif answer[1] < len(current_path):
+#                             answer[0] = child
+#                             answer[1] = len(current_path)
+#                         # if the edge length of the existing answer (answer[1]) equals the current path length, we overwrite it if the current ancestor integer saved in 'child' is lower than the one currently saved in answer[0].
+#                         elif answer[1] == len(current_path) and answer[0] > child:
+#                             answer[0] = child
+#                             answer[1] = len(current_path)
+                    
+#                     # Add the node we are looking at to our visited set if we haven't worked with it yet
+#                     if current_node not in visited_2:
+#                         visited_2.add(current_node)
+
+#                         # If the current node has connections, we toss those on the stack and continue the loop
+#                         if current_node in ancestor_dictionary:
+#                             edges = ancestor_dictionary[current_node]
+#                             for edge in edges:
+#                                 stack_2.push(current_path + [edge])
+                                
+#                 # Once we've checked everything in the stack, break the loop and continue with the outer reverse DFS functionality
+#                 break
+
+#         # Check if there are parents to the current child node
+#         # If there are parents that have not been visited, add the to the stack
+#         for item in ancestor_dictionary:
+#             if child in ancestor_dictionary[item] and item not in visited:
+#                 visited.add(item)
+#                 stack.push(item)
+
+#     # Return earliest answers. If there are no parents, return -1
+#     if answer[0] is not None:
+#         return answer[0]
+#     else:
+#         return -1
+
+
+
 
 def earliest_ancestor(ancestors, starting_node):
-    # Answer will hold the earliest ancestor answer[0] and the amount of edges to reach it answer[1]
-    answer = [None, None]
+
+    earliest_ancestor = -1
+    current_length = 0
 
     # Adjacency list for all nodes/edges in ancestors
     ancestor_dictionary = {}
@@ -27,66 +121,27 @@ def earliest_ancestor(ancestors, starting_node):
         else:
             ancestor_dictionary[a[0]] = set()
             ancestor_dictionary[a[0]].add(a[1])
-
-    # Set up a reverse depth first search of the nodes.
-    stack = Stack()
+    
+    # Begin BFS
+    queue = Queue()
     visited = set()
-    stack.push(starting_node)
-    while stack.size() > 0:
-        child = stack.pop()
+    queue.enqueue([starting_node])
+    while queue.size() > 0:
+        current_path = queue.dequeue()
+        current_node = current_path[-1]
+        if current_node != starting_node:
+            if len(current_path) > current_length:
+                earliest_ancestor = current_node
+                current_length = len(current_path)
+            elif len(current_path) == current_length and earliest_ancestor > current_node:
+                earliest_ancestor = current_node
+        if current_node not in visited:
+            visited.add(current_node)
+            edges = []
+            for x in ancestor_dictionary:
+                if current_node in ancestor_dictionary[x] and x not in visited:
+                    edges.append(x)
+            for edge in edges:
+                queue.enqueue(current_path + [edge])
 
-
-        # If we are currently not on the first node, do a top down DFS of the nodes to find the length of any connecting path to the original child
-        if child != starting_node:
-
-            while True:
-                stack_2 = Stack()
-                visited_2 = set()
-                stack_2.push([child])
-                while stack_2.size() > 0:
-                    current_path = stack_2.pop()
-                    current_node = current_path[-1]
-                    
-                    #Here we have reached the base node
-                    if current_node == starting_node:
-                        # If nothing has been put in answer, we use the current value of 'child' as our earliest ancestor.
-                        # We also set the length of the path to indicate how many edges there are to get there.
-                        if answer[0] is None:
-                            answer[0] = child
-                            answer[1] = len(current_path)
-                        
-                        # If an answer already exists, we check to see if the current path is longer than the existing answer.
-                        # If the current path is longer, we overwrite the ancestor and edge length
-                        elif answer[1] < len(current_path):
-                            answer[0] = child
-                            answer[1] = len(current_path)
-                        # if the edge length of the existing answer (answer[1]) equals the current path length, we overwrite it if the current ancestor integer saved in 'child' is lower than the one currently saved in answer[0].
-                        elif answer[1] == len(current_path) and answer[0] > child:
-                            answer[0] = child
-                            answer[1] = len(current_path)
-                    
-                    # Add the node we are looking at to our visited set if we haven't worked with it yet
-                    if current_node not in visited_2:
-                        visited_2.add(current_node)
-
-                        # If the current node has connections, we toss those on the stack and continue the loop
-                        if current_node in ancestor_dictionary:
-                            edges = ancestor_dictionary[current_node]
-                            for edge in edges:
-                                stack_2.push(current_path + [edge])
-                                
-                # Once we've checked everything in the stack, break the loop and continue with the outer reverse DFS functionality
-                break
-
-        # Check if there are parents to the current child node
-        # If there are parents that have not been visited, add the to the stack
-        for item in ancestor_dictionary:
-            if child in ancestor_dictionary[item] and item not in visited:
-                visited.add(item)
-                stack.push(item)
-
-    # Return earliest answers. If there are no parents, return -1
-    if answer[0] is not None:
-        return answer[0]
-    else:
-        return -1
+    return earliest_ancestor
